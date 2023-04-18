05bec8e7a38b7b19625042b8d6bd0c7fc58a1d61
Sonja Ek <sonja3k@icloud.com>
2023-04-18 09:32:06 +0300

    Task 3.4: Add an endpoint for deleting notes + VSCode REST client test

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = contactInfo.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
